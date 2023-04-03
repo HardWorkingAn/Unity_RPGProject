fd9a57456f991358c5a65c92d9bda93510a9b59d
HardWorkingAn <agc780@naver.com>
2023-04-03 16:24:44 +0900

    feat : 플레이어 스탯 정보 추가

---
+++ Assets/C#/PlayerStat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int _Hp;
+    int _Exp;
+
+    void Start()
+    {
+        _Hp = 10;
+        _Exp = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
